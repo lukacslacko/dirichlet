06636457fda2c199b895bb91421c02bcb5ab27ef
lukacslacko <31095308+lukacslacko@users.noreply.github.com>
2025-06-08 08:51:50 +0200

    Update Four.lean

--- first/Four.lean
+++ first/Four.lean
@@ -5,7 +5,11 @@ import Mathlib
 
 open Nat Finset BigOperators Set
 
-lemma sum_le_log (k : Nat) : ∑ i ∈ range (2 ^ k), (1 : Real) / (i + 1) ≤ (k + 1) := by
+def r1 : Real := 1
+def r2 : Real := 2
+
+lemma sum_le_log (k : Nat) : ∑ i ∈ range (2 ^ k), r1 / (i + 1) ≤ (k + 1) := by
+  unfold r1
   induction k
   case zero => simp
   case succ k hk =>
@@ -29,18 +33,20 @@ lemma sum_le_log (k : Nat) : ∑ i ∈ range (2 ^ k), (1 : Real) / (i + 1) ≤ (
       simp
 
     calc
-      ∑ x ∈ b \ a, (1 : Real) / (x + 1) = ∑ x ∈ c, (1 : Real) / (x + 1) := by rfl
+      ∑ x ∈ b \ a, r1 / (x + 1) = ∑ x ∈ c, r1 / (x + 1) := by rfl
       _ ≤ ∑ x ∈ c, 1 / (2 ^ k) := by
         apply Finset.sum_le_sum
         intro i hi
         apply le_of_one_div_le_one_div
         simp
+        unfold r1
         field_simp
         exact_mod_cast le_succ_of_le (h_lower i hi)
       _ = #c • (1 / (2 ^ k)) := by rw [Finset.sum_const]
       _ = 1 := by rw [h_card]; field_simp
 
-lemma sum_ge_half_log (k : Nat) : ∑ i ∈ range (2 ^ k), (1 : Real) / (i + 1) ≥ (k + 1) / (2 : Real) := by
+lemma sum_ge_half_log (k : Nat) : ∑ i ∈ range (2 ^ k), r1 / (i + 1) ≥ (k + 1) / r2 := by
+  unfold r1 r2
   induction k
   case zero => norm_num
   case succ k hk =>
@@ -66,36 +72,87 @@ lemma sum_ge_half_log (k : Nat) : ∑ i ∈ range (2 ^ k), (1 : Real) / (i + 1) 
       rw [Finset.card_sdiff a_sub_b, Finset.card_range, Finset.card_range, pow_succ, mul_two]
       simp
 
-    have h_recip_lower: ∀ i ∈ c, (1 : Real) / (i + 1) ≥ (1 : Real) / (2 ^ (k + 1)) := by
+    have h_recip_lower: ∀ i ∈ c, r1 / (i + 1) ≥ r1 / (2 ^ (k + 1)) := by
       intro i hi
       apply le_of_one_div_le_one_div
+      unfold r1
       simp
       exact_mod_cast Nat.zero_lt_succ i
+      unfold r1
       field_simp
       have : i + 1 ≤ 2 ^ (k + 1) := by
         exact h_upper i hi
       exact_mod_cast this
 
     calc
-      ∑ i ∈ c, (1 : Real) / (i + 1)
-      ≥ ∑ i ∈ c, (1 : Real) / (2 ^ (k + 1)) := by
+      ∑ i ∈ c, r1 / (i + 1)
+      ≥ ∑ i ∈ c, r1 / (2 ^ (k + 1)) := by
         apply Finset.sum_le_sum h_recip_lower
-      _ = #c • ((1 : Real) / (2 ^ (k + 1))) := by
+      _ = #c • (r1 / (2 ^ (k + 1))) := by
         rw [Finset.sum_const]
-      _ = (2 ^ k) • ((1 : Real) / (2 ^ (k + 1))) := by
+      _ = (2 ^ k) • (r1 / (2 ^ (k + 1))) := by
         rw [h_card]
-      _ = (2 ^ k) * ((1 : Real) / (2 ^ (k + 1))) := by
+      _ = (2 ^ k) * (r1 / (2 ^ (k + 1))) := by
+        unfold r1
         simp
-      _ = (2 ^ k : Real) / (2 ^ (k + 1)) := by
+      _ = 2 ^ k / (2 ^ (k + 1)) := by
+        unfold r1
         ring
-      _ = (2 ^ k : Real) / (2 ^ k * 2) := by
+      _ = 2 ^ k / (2 ^ k * 2) := by
         rw [pow_succ]
-      _ = (1 : Real) / 2 := by
+      _ = r1 / 2 := by
+        unfold r1
         field_simp
 
 theorem sum_between_log (k : Nat) :
-  (k + 1) / (2 : Real) ≤ ∑ i ∈ range (2 ^ k), (1 : Real) / (i + 1) ∧
-  ∑ i ∈ range (2 ^ k), (1 : Real) / (i + 1) ≤ (k + 1) := by
+  (k + 1) / r2 ≤ ∑ i ∈ range (2 ^ k), r1 / (i + 1) ∧
+  ∑ i ∈ range (2 ^ k), r1 / (i + 1) ≤ (k + 1) := by
   constructor
   exact sum_ge_half_log k
   exact sum_le_log k
+
+theorem sum_square_lt_two (k : Nat) :
+  ∑ i ∈ range k, r1 / (i + 1) ^ 2 < r2 := by
+    cases k
+    case zero =>
+      unfold r1 r2
+      norm_num
+    case succ k =>
+    set f1 := fun (i : Nat) => r1 / (i + 1) ^ 2
+    set f2 := fun (i : Nat) => r1 / (i * (i + 1))
+    set f3 := fun (i : Nat) => r1 / (i + 1)
+    have f1_le_f2 (i : Nat) (hi : i > 0) : f1 i ≤ f2 i := by
+      unfold f1 f2 r1
+      gcongr
+      nlinarith
+    have f2_eq_f3 (i : Nat) : f2 (i + 1) = f3 i - f3 (i + 1) := by
+      unfold f2 f3 r1
+      field_simp
+    unfold r2
+
+    rw [Finset.sum_range_succ']
+    unfold r1
+    field_simp
+    have : ∑ i ∈ range k, f1 (i + 1) < 1 := by
+      calc
+        ∑ i ∈ range k, f1 (i + 1) ≤ ∑ i ∈ range k, f2 (i + 1) := by
+          apply Finset.sum_le_sum
+          intro i hi
+          apply f1_le_f2
+          linarith
+        _ = ∑ i ∈ range k, (f3 i - f3 (i + 1)) := by
+          apply Finset.sum_congr
+          rfl
+          intro i hi
+          apply f2_eq_f3
+        _ = f3 0 - f3 k := by
+          apply sum_range_sub'
+      unfold f3 r1
+      field_simp
+      refine (div_lt_one ?_).mpr ?_
+      linarith
+      simp
+    unfold f1 r1 at this
+    norm_num at this
+    simp
+    linarith
